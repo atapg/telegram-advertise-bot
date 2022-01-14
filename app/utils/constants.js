24511f6aaac9e59098b16726c1da8c598ed2e5b3
ata parvin ghods <ataparvinghods@gmail.com>
2022-01-14 19:49:36 +0330

    Added welcome text

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,6 @@
+const botName = 'بات آگهی تستی'
+const botId = '@test_kelt_bot'
+const channelId = '@test_kelt'
 const addAdv = 'ثبت آگهی 📝'
 const manageAdvs = 'آگهی های من 📰'
 const returnFromAdvsScene = 'بازگشت ⬅️'
@@ -6,4 +9,7 @@ module.exports = {
 	addAdv,
 	manageAdvs,
 	returnFromAdvsScene,
+	botId,
+	botName,
+	channelId,
 }
